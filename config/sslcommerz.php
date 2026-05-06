b67a35a52a292ea8f62c05026dfe75bd6aec6e34
zobay <aszubayr@gmail.com>
2026-05-06 18:08:36 +0600

    Update Service provider with sslcommerz config

--- config/sslcommerz.php
+++ config/sslcommerz.php
@@ -2,5 +2,16 @@
 
 // config for Zobay/LaravelSslCommerz
 return [
-
+    'sandbox'          => env('SSLCOMMERZ_SANDBOX', true),
+    'sandbox_base_url' => 'https://sandbox.sslcommerz.com',
+    'live_base_url'    => 'https://securepay.sslcommerz.com',
+    'version'          => 'v4',
+    'credentials' => [
+        'store_id'     => env('SSLCOMMERZ_STORE_ID'),
+        'store_passwd' => env('SSLCOMMERZ_STORE_PASSWD'),
+    ],
+    'success_url' => env('SSLCOMMERZ_SUCCESS_URL'),
+    'fail_url'    => env('SSLCOMMERZ_FAIL_URL'),
+    'cancel_url'  => env('SSLCOMMERZ_CANCEL_URL'),
+    'ipn_url'     => env('SSLCOMMERZ_IPN_URL'),
 ];
--- src/LaravelSslCommerzServiceProvider.php
+++ src/LaravelSslCommerzServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Zobay\LaravelSslCommerz;
 
+use Illuminate\Support\Facades\Http;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Zobay\LaravelSslCommerz\Commands\LaravelSslCommerzCommand;
@@ -21,5 +22,29 @@ class LaravelSslCommerzServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasMigration('create_laravel_sslcommerz_table')
             ->hasCommand(LaravelSslCommerzCommand::class);
+    }
+
+    public function packageBooted(): void
+    {
+        Http::macro('sslcommerz', function () {
+            $baseUrl = config('sslcommerz.sandbox')
+                ? config('sslcommerz.sandbox_base_url')
+                : config('sslcommerz.live_base_url');
+
+            $version = config('sslcommerz.version');
+
+            return Http::baseUrl("{$baseUrl}/gwprocess/{$version}")
+                ->withHeaders([
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'Accept'       => 'application/json',
+                ]);
+        });
+
+        if ($this->app->runningInConsole()) {
+
+            $this->publishes([
+                __DIR__.'/../config/sslcommerz.php' => config_path('sslcommerz.php'),
+            ], 'config');
+        }
     }
 }
